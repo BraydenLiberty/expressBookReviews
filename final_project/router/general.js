832207ebd1a719f8bacc59e0e099a47a51d4854c
BraydenLiberty <bcloutier@liberty.edu>
2024-04-24 15:35:25 -0400

    Final Complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,7 +5,7 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => { 
+public_users.post("/register", (req,res) => {  
     const username = req.body.username;
     const password = req.body.password;
     if (username && password) {
@@ -21,39 +21,68 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
+    let allBooksPromise = new Promise((resolve,reject) => {
+        res.send(JSON.stringify(books,null,4));
+        resolve("Get all books promise resolved");
+    })
+    
+    allBooksPromise.then((successMessage) => {
+        console.log(successMessage);
+    })
 });
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn]) 
+    let byISBNPromise = new Promise((resolve,reject) => {
+        res.send(books[isbn]);
+        resolve("Get by isbn promise resolved");
+    })
+
+    byISBNPromise.then((successMessage) => {
+        console.log(successMessage);
+    })
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
-    for(let key in books) {
-        if(books.hasOwnProperty(key)) {
-            let book = books[key];
-            if (book.author === author) {
-                res.send(book)
+    let searchByAuthorPromise = new Promise((resolve,reject) => {
+        for(let key in books) {
+            if(books.hasOwnProperty(key)) {
+                let book = books[key];
+                if (book.author === author) {
+                    res.send(book)
+                }
             }
         }
-    }
+        resolve("Retrieve by author promise resolved")
+    })
+
+    searchByAuthorPromise.then((successMessage) => {
+        console.log(successMessage);
+    })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
-    for(let key in books) {
-        if(books.hasOwnProperty(key)) {
-            let book = books[key];
-            if (book.title === title) {
-                res.send(book)
+    let searchByTitlePromise = new Promise((resolve,reject) => {
+        for(let key in books) {
+            if(books.hasOwnProperty(key)) {
+                let book = books[key];
+                if (book.title === title) {
+                    res.send(book)
+                }
             }
         }
-    }
+        resolve("Retrieve by title promise resolved");
+    })
+
+    searchByTitlePromise.then((successMessage) => {
+        console.log(successMessage);
+    })
 });
 
 //  Get book review
